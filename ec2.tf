1cc2237f26dd9c3050f6bed3e94894f56536ce4d
code with J3 <jatinjangel27@gmail.com>
2026-03-18 18:39:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,8 +5,7 @@ provider "aws" {
 resource "aws_instance" "cbz_ec2" {
   ami           = "ami-080254318c2d8932f"
   instance_type = "t3.micro"
-  key_name      = "newacckey.pem"
-
+  
   tags = {
     Name = "cbz-ubuntu-ec2"
   }
